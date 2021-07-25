1aea36def3d1a688130b2c666fc27edd5172344f
johnturner4004 <johnturner4004@gmail.com>
2021-07-25 19:37:08 -0400

    cleaned up scripts and object list; added comments

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour
 {
+  // game object used to reverence vehicle in the camera position
   public GameObject player;
+  //Offset to keep camera above and behind the vehicle
   private Vector3 offset = new Vector3(0, 5, -8);
 
   // Start is called before the first frame update
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-  public float speed = 5.0f;
-  public float turnSpeed = 2.0f;
-  public float horizontalInput;
-  public float forwardInput;
+  //private variables used for player control
+  private float speed = 15.0f;
+  private float turnSpeed = 30.0f;
+  private float horizontalInput;
+  private float forwardInput;
 
   // Start is called before the first frame update
   void Start()
@@ -24,6 +25,6 @@ public class PlayerController : MonoBehaviour
 
     //Move the vehicle left and right using player input
     horizontalInput = Input.GetAxis("Horizontal");
-    transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
+    transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
   }
 }
